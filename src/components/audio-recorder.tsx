d9ee315780a3989811a6ba51fc3d1fc5962f2fb6
Axl Crawford <hi@axlcrawford.com>
2024-09-26 22:55:20 +0100

    Scaffold

---
+++ src/components/audio-recorder.tsx
@@ -0,0 +1,10 @@
+import { useWebAudio } from "../hooks/useWebAudio";
+
+export function AudioRecorder() {
+  return (
+    <div>
+      <button>Play</button>
+      <button>Stop</button>
+    </div>
+  )
+}
