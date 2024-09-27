0eb995b3c971e2a092a46b343866e13a32c98f22
Axl Crawford <hi@axlcrawford.com>
2024-09-27 11:45:25 +0100

    working version of audio recorder

--- src/components/audio-recorder.tsx
+++ src/components/audio-recorder.tsx
@@ -1,10 +1,38 @@
+import { useRef } from "react";
 import { useWebAudio } from "../hooks/useWebAudio";
 
 export function AudioRecorder() {
+  const startBtnRef = useRef<HTMLButtonElement | null>(null);
+  const stopBtnRef = useRef<HTMLButtonElement | null>(null);
+  const audioPlaybackRef = useRef<HTMLAudioElement | null>(null);
+
+  const { isRecording, status, audioUrl, startRecording, stopRecording } = useWebAudio();
+
   return (
     <div>
-      <button>Play</button>
-      <button>Stop</button>
+      <div>
+        <button
+          ref={startBtnRef}
+          onClick={startRecording}
+          disabled={isRecording}
+        >
+          Start Recording
+        </button>
+        <button
+          ref={stopBtnRef}
+          onClick={stopRecording}
+          disabled={!isRecording}
+        >
+          Stop Recording
+        </button>
+      </div>
+      <audio
+        ref={audioPlaybackRef}
+        controls
+        src={audioUrl ?? undefined}
+        hidden={!audioUrl}
+      />
+      <div id="status">{status}</div>
     </div>
   )
 }
