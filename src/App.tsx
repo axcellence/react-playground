64fd597b431030d186ba1e5952b260f8871a7b71
Axl Crawford <hi@axlcrawford.com>
2024-09-29 17:52:24 +0100

    Add geolocation hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { AudioRecorder } from './components/audio-recorder'
 import { ShareButton } from './components/webshare'
+import { Geolocation } from './components/geolocation/geolocation'
 
 function App() {
 
   return (
     <>
       {/* <ShareButton /> */}
-      <AudioRecorder />
+      {/* <AudioRecorder /> */}
+      <Geolocation />
     </>
   )
 }
